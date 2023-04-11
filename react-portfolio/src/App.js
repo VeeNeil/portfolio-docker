0fab022ab50861587a75fa2ad6d18ec39735a49a
VeeNeil <80906756+VeeNeil@users.noreply.github.com>
2023-04-11 20:09:33 +0700

    update

---
+++ react-portfolio/src/App.js
@@ -0,0 +1,15 @@
+import './App.scss';
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './components/Home';
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element= {<Layout />}/>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
