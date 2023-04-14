1353430dde171add684a47189e93ce162ff30916
VeeNeil <80906756+VeeNeil@users.noreply.github.com>
2023-04-15 05:35:54 +0700

    4/15/2023 update

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,12 +1,14 @@
 import './App.scss';
-import { Sidebar} from './components/Sidebar';
-import { Banner} from './components/Banner';
+import { Sidebar } from './components/Sidebar';
+import { Banner } from './components/Banner';
+import { Skills } from './components/Skills';
 import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <div className= "App">
       <Sidebar/>
       <Banner/>
+      <Skills/>
     </div>
   );
 }
